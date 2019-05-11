01244d05cc2ad1ae8714affae300a73a7dd5d2f8
hsm-hx <skytea922@gmail.com>
2019-05-12 00:11:54 +0900

    :innocent: addを関数にする必要，無し！ｗ

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int add(int x, int y){
-  return x + y;
-}
-
 int main(void){
   int a, b;
   cin >> a >> b;
 
-  int sum = add(a, b);
+  int sum = a + b;
 
   cout << sum << endl;
   return 0;
