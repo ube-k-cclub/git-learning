6849e4d04f108c74ee388e656ab7c60718d215b0
hsm-hx <skytea922@gmail.com>
2019-05-12 00:18:28 +0900

    :heavy_plus_sign: 総額表示用の関数を追加

--- main.cpp
+++ main.cpp
@@ -5,13 +5,19 @@ int tax(int x){
   return x * 0.08; // 消費税のみの金額を返す
 }
 
+void casher(int x){
+  cout << "お支払金額は";
+  cout << tax(x);
+  cout << "円です" << endl;
+}
+
 int main(void){
   int a, b;
   cin >> a >> b;
 
   int sum = a + b;
-  sum = tax(sum);
 
-  cout << sum << endl;
+  casher(sum);
+
   return 0;
 }
