190c012199dd00cf592116b92a64b518a3ac720b
hsm-hx <skytea922@gmail.com>
2019-05-12 00:13:05 +0900

    :heavy_plus_sign: 消費税を計算するようにした

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int tax(int x){
+  return x * 1.08;
+}
+
 int main(void){
   int a, b;
   cin >> a >> b;
 
   int sum = a + b;
+  sum = tax(sum);
 
   cout << sum << endl;
   return 0;
